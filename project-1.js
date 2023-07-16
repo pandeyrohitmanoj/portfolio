da8897e51360669bc8efecf7dbaa2331ba2519ee
pandeyrohitmanoj <41313712+pandeyrohitmanoj@users.noreply.github.com>
2023-07-16 11:15:19 +0530

    update bottom bar

--- project-1.js
+++ project-1.js
@@ -3,3 +3,22 @@
     }
     
     
+  function reachSocialMediaForIcon( iconName ){
+   switch(iconName){
+     case "youtube":
+     window.location.href='https://www.youtube.com/channel/UChztW3uF4e6kqO--ZS-zbHA'
+     break
+     case "facebook":
+     window.location.href='https://www.facebook.com/profile.php?id=100021839953940'
+     break
+     case "instagram":
+     window.location.href='https://www.instagram.com/rohit_red_arcade/'
+     break
+     case "twitter":
+     window.location.href='https://twitter.com/pandeyg42848875'
+     break
+     case "linkedin":
+     window.location.href='https://www.linkedin.com/in/rohit-pandey-3a4354167/'
+     break
+   }
+ }
